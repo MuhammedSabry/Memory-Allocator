e5e4be9953a3c208ba0e3fd43deb8a3ecdb649df
Muhammed Sabry <muhammed.sabry.refai@gmail.com>
2018-04-26 15:04:30 +0200

    Added controllers and util methods

--- src/sample/controller/MemoryController.java
+++ src/sample/controller/MemoryController.java
@@ -1,11 +1,15 @@
 package sample.controller;
 
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class MemoryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MemoryController  implements Initializable {
 
 
     public Button memory_next;
@@ -13,4 +17,9 @@ public class MemoryController {
     public TextField memory_size;
     public Button memory_add;
     public ListView memory_list;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/sample/controller/ProcessesController.java
+++ src/sample/controller/ProcessesController.java
@@ -1,14 +1,23 @@
 package sample.controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class ProcessesController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProcessesController implements Initializable {
 
     public Button process_next;
     public TextField process_name;
     public TextField process_size;
     public Button process_add;
     public ListView process_list;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
